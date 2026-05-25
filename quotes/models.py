161987db494ff314039c7acb43038bb2f1dfb6e0
AsmitaTimalsena <020bscit011@sxc.edu.np>
2026-05-25 19:50:57 +0545

    Added PostgreSQL database config

--- quotes/models.py
+++ quotes/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = [
+    ('Motivation','Motivation'),
+    ('Success','Success'),
+    ('Life','Life'),
+
+]
 class Quote(models.Model):
     text = models.TextField()
     author = models.CharField(max_length=100)
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default='Motivation'
+    )
 
     def __str__(self):
         return self.author
