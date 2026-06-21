d102740fcf5245e7fe740f8b7f05276d7e80cfaf
AsmitaTimalsena <020bscit011@sxc.edu.np>
2026-06-21 16:59:06 +0545

    improving views.py

--- quote_api/settings.py
+++ quote_api/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 import os
 
 
-GEMINI_KEY = os.getenv("GEMINI_API_KEY")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,6 +35,7 @@ else:
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
+GEMINI_KEY = os.getenv("GEMINI_API_KEY")
 
 ALLOWED_HOSTS = ['*']
 
